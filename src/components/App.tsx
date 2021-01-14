a6475bbab01e3802218ea4d83975d2e355af8d99
Selena Siri <selenasiri@gmail.com>
2021-01-14 13:55:25 -0500

    counter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,13 +4,26 @@ interface AppProps {
   color: string;
 }
 
-// const App = (props: AppProps) => {
-  const App: React.FC<AppProps> = (props: AppProps): JSX.Element => { // JSX.Element is a return type annotation
-  return (
-    <div>
-      {props.color}
-    </div>
+const App : React.FC<AppProps> = (props) => { 
+  const [counter, setCounter] = React.useState<number>(0)
+
+  const onIncrement = (): void => {
+    setCounter((prevCounter)=> prevCounter + 1)
+  }
+
+  const onDecrement = (): void => {
+    setCounter((prevCounter)=> prevCounter - 1)
+  }
+
+    return (
+      <div>
+        <button onClick={onIncrement}>Increment</button>
+        <button onClick={onDecrement}>Decrement</button>
+        {counter}
+      </div>
   )
 }
 
 export default App
+
+
